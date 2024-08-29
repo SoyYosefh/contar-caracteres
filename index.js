ae4d3acb4e894dda377600a5fa6ad062c7995c8c
SoyYosefh <joratejedana@gmail.com>
2024-08-28 17:29:58 -0700

    Subida de archivos

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1- Conteo de caracteres: Dada una cadena de texto 
+// muestra el número de caracteres que contiene esa cadena.
+
+const texto = 'Hola mundo';
+
+const caracteres = texto.length;
+console.log(`El texto tiene '${caracteres}' caracteres`);
